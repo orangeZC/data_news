8b932fb61e4dfd2f389f74b659d6d92ef614d8ef
orange_zc <15826758321@163.com>
2018-03-10 19:44:02 +0800

    Update qc_news.py

--- qc_news.py
+++ qc_news.py
@@ -9,10 +9,10 @@ from create_id import create_id
 
 def qc_spider(url, params, header):
     conn = pymysql.connect(
-        host='111.230.149.22',
+        host='0.0.0.0',
         port=3306,
-        user='root',
-        password='liu3226677',
+        user='username',
+        password='password',
         db='data_news',
         charset='utf8'
     )
